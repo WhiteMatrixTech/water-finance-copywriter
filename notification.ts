b616fddb8c8d5d362f117dc44e3da53d685bcb62
Liyw-liyw <liyuanwu_liyw@163.com>
2023-02-03 10:21:32 +0800

    feat: add notification text

---
+++ notification.ts
@@ -0,0 +1,16 @@
+export default {
+  zh: {
+    sendVerifyCodeError: '验证码发送错误',
+    bindEmailError: '邮箱绑定错误',
+    touchSubmitSuccess: '提交成功',
+    touchSubmitSError: '提交失败',
+    touchSubmitSuccessDesc: '我们的团队会尽快与您联系'
+  },
+  en: {
+    sendVerifyCodeError: 'Verification Code Send Error',
+    bindEmailError: 'Email Bind Error',
+    touchSubmitSuccess: 'Submit successfully',
+    touchSubmitSError: 'Submit Error',
+    touchSubmitSuccessDesc: 'Our team will be in touch as soon as possible'
+  }
+}
--- resources.ts
+++ resources.ts
@@ -1,17 +1,20 @@
 import global from './global'
 import home from './home'
+import notification from './notification'
 
 export const resources = {
   en: {
     translation: {
       en: 'English',
-      ...home.en
+      ...home.en,
+      ...notification.en
     }
   },
   zh: {
     translation: {
       zh: '简体中文',
-      ...home.zh
+      ...home.zh,
+      ...notification.zh
     }
   },
   global
