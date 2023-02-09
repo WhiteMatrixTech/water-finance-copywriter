535a31a895221a36ab12f57c3fabd9e66f294d90
Liyw-liyw <liyuanwu_liyw@163.com>
2023-02-09 17:24:31 +0800

    feat: update notification

--- notification.ts
+++ notification.ts
@@ -4,13 +4,19 @@ export default {
     bindEmailError: '邮箱绑定错误',
     touchSubmitSuccess: '提交成功',
     touchSubmitSError: '提交失败',
-    touchSubmitSuccessDesc: '我们的团队会尽快与您联系'
+    touchSubmitSuccessDesc: '我们的团队会尽快与您联系',
+    signSubmitSuccess: '签名请求成功',
+    signSubmitSError: '签名请求失败',
+    signSubmitSuccessDesc: '请前往您的邮箱查看进行Dropbox签名'
   },
   en: {
     sendVerifyCodeError: 'Verification Code Send Error',
     bindEmailError: 'Email Bind Error',
     touchSubmitSuccess: 'Submit successfully',
     touchSubmitSError: 'Submit Error',
-    touchSubmitSuccessDesc: 'Our team will be in touch as soon as possible'
+    touchSubmitSuccessDesc: 'Our team will be in touch as soon as possible',
+    signSubmitSuccess: 'Sign submit successfully',
+    signSubmitSError: 'Sign submit Error',
+    signSubmitSuccessDesc: 'Please go to your mailbox to check for Dropbox signature'
   }
 }
