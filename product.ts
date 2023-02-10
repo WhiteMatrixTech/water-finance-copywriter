540493ed5b445d0a3d62dda0190b0dcdf593ed5e
Liyw-liyw <liyuanwu_liyw@163.com>
2023-02-10 10:17:13 +0800

    feat: add product data model

---
+++ product.ts
@@ -0,0 +1,40 @@
+export default {
+  zh: {
+    'ProductName': '产品名称',
+    'DealOverview': '产品介绍',
+    'ProductID': '用于记录和唯一标识产品',
+    'ProductStatus': '产品募集的状态',
+
+    'Pool Value': '需要募集的USDT总值',
+    'USDT APY': '池的总估计固定 USDT 收益率',
+    'Principal Outstanding': '借款人在其付款期限内偿还给该池的剩余本金总额',
+    'Payment Frequency': '支付利息的频率',
+    'Next Repayment Due': '根据 Pool 的交易条款，借款人向该 Pool 还款的下一个预定日期',
+    'Payment Term': '全额本金到期前的时间长度',
+    'Payment Term End': '根据 Pool 的交易条款，Pool 的付款期限结束的日期，以及借款人计划在该日期之前全额偿还其贷款总额的日期',
+    'Opening Date': '池对支持者投资开放的日期',
+    'Due Date': '池对支持者结束投资开放的日期',
+    'Contract Address': '合约地址',
+    'Borrower Address': '借款人钱包地址',
+    'Drawdown Cap': '借款人可以从该资金池中提取的资金总额'
+  },
+  en: {
+    'ProductName': 'product name',
+    'DealOverview': 'deal overview',
+    'ProductID': 'product id',
+    'ProductStatus': 'product status',
+
+    'Pool Value': "The Pool's total USDT value",
+    'USDT APY': "The Pool's total estimated fixed-rate USDT APY",
+    'Principal Outstanding': 'The total amount of principal remaining for the Borrower to repay to this Pool over its payment term.',
+    'Payment Frequency': 'Frequency of interest payments.',
+    'Next Repayment Due': "The next scheduled date for the Borrower to make a repayment to this Pool, according to the Pool's deal terms.",
+    'Payment Term': 'The length of time until the full principal is due.',
+    'Payment Term End': "The date that the Pool's payment term will end, and by which the Borrower is scheduled to have repaid their total loan amount in full, according to the Pool's deal terms.",
+    'Opening Date': 'The date that the Pool will be open for Backer investments.',
+    'Due Date': 'The date that the Pool will be closed for Backer investments.',
+    'Contract Address': 'The Ethereum address for this Borrower Pool smart contract.',
+    'Borrower Address': 'The Ethereum address associated with this Borrower.',
+    'Drawdown Cap': 'The total funds that the Borrower can drawdown from this Pool.'
+  }
+}
--- resources.ts
+++ resources.ts
@@ -1,12 +1,14 @@
 import global from './global'
 import home from './home'
 import notification from './notification'
+import product from './product'
 
 export const resources = {
   en: {
     translation: {
       en: 'English',
       ...home.en,
+      ...product.en,
       ...notification.en
     }
   },
@@ -14,6 +16,7 @@ export const resources = {
     translation: {
       zh: '简体中文',
       ...home.zh,
+      ...product.zh,
       ...notification.zh
     }
   },
