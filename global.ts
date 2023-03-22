5fbcba3e37006f33d2dbf2c9720180935b578b98
Liyw-liyw <liyuanwu_liyw@163.com>
2023-03-22 10:02:12 +0800

    feat: add s3 base uri & document uri

--- global.ts
+++ global.ts
@@ -1,4 +1,9 @@
 export default {
+  s3BaseUri: 'https://dsxzoz6wy2sxi.cloudfront.net',
+  document: {
+    termsOfService: 'https://dsxzoz6wy2sxi.cloudfront.net/doc/Privacy_Policy.pdf',
+    privacyPolicy: 'https://dsxzoz6wy2sxi.cloudfront.net/doc/Terms_of_service.pdf'
+  },
   social: {
       discord: 'https://discord.gg/ezekclub',
       twitter: 'https://twitter.com/EzekClub',
